f150de3a68b008f08c426c9eeae1b5b47a88e03c
Naimish <rishunaimish@gmail.com>
2024-07-15 16:35:07 +0530

    Update SceneSwitcherEditor.cs

--- SceneSwitcherEditor.cs
+++ SceneSwitcherEditor.cs
@@ -1,17 +1,21 @@
 //It lists down all the scenes in your project.
 //The list is updated as soon as a scene is deleted or created.
 //You can Locate, Open or Delete the Scene
+//You can also add scenes into favourites
 
-using UnityEditor;
-using UnityEngine;
+using System.Collections.Generic;
 using System.IO;
-using UnityEditor.SceneManagement;
 using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
 
 public class SceneGridEditorWindow : EditorWindow
 {
     private Vector2 scrollPosition;
+    private Vector2 favouritesScrollPosition;
     private string sceneSearchFilter = "";
+    private List<string> favouriteScenes = new List<string>();
 
     [MenuItem("Window/Scene Quick Access")]
     private static void ShowWindow()
@@ -23,7 +27,51 @@ public class SceneGridEditorWindow : EditorWindow
     {
         GUILayout.Label("Scene Quick Access", EditorStyles.boldLabel);
 
+        EditorGUILayout.Space(10, true);
+
         sceneSearchFilter = EditorGUILayout.TextField("Search", sceneSearchFilter);
+
+        EditorGUILayout.Space(10, true);
+
+        // Favourites subwindow
+        GUILayout.Label("Favourites", EditorStyles.boldLabel);
+        favouritesScrollPosition = EditorGUILayout.BeginScrollView(favouritesScrollPosition, GUILayout.Height(225));
+        GUILayout.BeginHorizontal();
+        if (favouriteScenes.Count == 0)
+        {
+            GUILayout.FlexibleSpace();
+            GUILayout.BeginVertical();
+            GUILayout.FlexibleSpace();
+            GUILayout.Label("Click on Favourite to add a scene", EditorStyles.boldLabel);
+            GUILayout.FlexibleSpace();
+            GUILayout.EndVertical();
+            GUILayout.FlexibleSpace();
+        }
+        else
+        {
+            foreach (var scene in favouriteScenes)
+            {
+                GUILayout.BeginVertical();
+                // Display scene icon
+                Texture2D sceneIcon = EditorGUIUtility.IconContent("SceneAsset Icon").image as Texture2D;
+                if (GUILayout.Button(sceneIcon, GUILayout.Width(150), GUILayout.Height(150)))
+                {
+                    EditorSceneManager.OpenScene(scene);
+                }
+                // Display scene name
+                GUILayout.Label(Path.GetFileNameWithoutExtension(scene), GUILayout.Width(150));
+                // Button to remove the scene from favourites
+                if (GUILayout.Button("Remove", GUILayout.Width(150)))
+                {
+                    favouriteScenes.Remove(scene);
+                    break; // Exit the loop to avoid modifying the collection while iterating
+                }
+                GUILayout.EndVertical();
+            }
+        }
+        GUILayout.EndHorizontal();
+        EditorGUILayout.EndScrollView();
+
 
         // Get all scenes in the build settings
         var scenes = AssetDatabase.FindAssets("t:Scene")
@@ -70,6 +118,16 @@ public class SceneGridEditorWindow : EditorWindow
                 {
                     AssetDatabase.DeleteAsset(scene);
                     AssetDatabase.Refresh();
+                }
+            }
+
+            // Button to add the scene to favourites
+            GUI.backgroundColor = Color.yellow;
+            if (GUILayout.Button("Favourite", GUILayout.Width(80)))
+            {
+                if (!favouriteScenes.Contains(scene))
+                {
+                    favouriteScenes.Add(scene);
                 }
             }
 
