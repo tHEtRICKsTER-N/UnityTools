3eb0f82bac93edd16867e26620ee5a68448bb413
Naimish <rishunaimish@gmail.com>
2024-07-16 12:55:50 +0530

    Update SceneSwitcherEditor.cs

--- SceneTools/SceneSwitcherEditor.cs
+++ SceneTools/SceneSwitcherEditor.cs
@@ -10,7 +10,7 @@ using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 
-public class SceneGridEditorWindow : EditorWindow
+public class SceneQuickAccessWindow : EditorWindow
 {
     private Vector2 scrollPosition;
     private Vector2 favouritesScrollPosition;
@@ -23,7 +23,7 @@ public class SceneGridEditorWindow : EditorWindow
     [MenuItem("Window/Scene Quick Access")]
     private static void ShowWindow()
     {
-        GetWindow<SceneGridEditorWindow>("Scene Quick Access");
+        GetWindow<SceneQuickAccessWindow>("Scene Quick Access");
     }
 
     private void OnEnable()
@@ -142,11 +142,19 @@ public class SceneGridEditorWindow : EditorWindow
                 }
             }
 
-            // Button to add the scene to favourites
-            GUI.backgroundColor = Color.yellow;
-            if (GUILayout.Button("Favourite", GUILayout.Width(80)))
+            // Determine if the scene is already in favourites
+            bool isFavourite = favouriteScenes.Contains(scene);
+
+            // Button to add/remove the scene from favourites
+            GUI.backgroundColor = isFavourite ? Color.gray : Color.yellow;
+            string favButtonLabel = isFavourite ? "Remove" : "Favourite";
+            if (GUILayout.Button(favButtonLabel, GUILayout.Width(80)))
             {
-                if (!favouriteScenes.Contains(scene))
+                if (isFavourite)
+                {
+                    favouriteScenes.Remove(scene);
+                }
+                else
                 {
                     favouriteScenes.Add(scene);
                 }
